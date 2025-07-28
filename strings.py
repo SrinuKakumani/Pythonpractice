9062203ad1b51d2b31cecc3b22903863e93e9928
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-28 18:43:39 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+#Get the character at position 7
+a = "Hello, World!"
+print(a[7])
+
+#loop through the characters in a string
+for a in "apple":
+    print(a)
+
+#To get the length of a string
+a = "Hello, World!"
+print(len(a))
+
+
+#To check if a certain phrase or character is present in a string
+x = "This is Srinu Kakumani from Kandukur"
+print("free" in x)
+
