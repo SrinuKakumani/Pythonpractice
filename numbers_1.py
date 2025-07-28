1edfbe18809c5a677dc0a373cdaee3a51fde2b30
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-28 18:22:50 +0530

    numbers

--- numbers_1.py
+++ numbers_1.py
@@ -5,3 +5,19 @@ z = -3255522
 print(type(x))
 print(type(y))
 print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3
+y = 5e3
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
