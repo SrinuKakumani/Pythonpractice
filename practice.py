10cb133ae59ef3d2ff0e3611d356e68c4a18c012
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-28 18:15:11 +0530

    practice

--- practice.py
+++ practice.py
@@ -1,2 +1,35 @@
-print("friends");
-print("reds")
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = "study"; #x is an string here
+x = 4; #x is an integer here 
+print(x)
+
+a=55;
+b=56;
+print(a+b)
+
+y = 456;
+t = 8.76;
+h = "dec";
+print(type(y));
+print(type(t));
+print(type(h));
+
+x, y, z = "Orange", "Banana", "Cherry";
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Orange";
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
