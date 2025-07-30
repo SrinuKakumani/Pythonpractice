f8cc777c7d032b082e65169f6431dc36c6c7544f
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-30 09:38:26 +0530

    string escape characters

--- strings.py
+++ strings.py
@@ -26,3 +26,37 @@ print(x[:7]);
 # To end at the end
 x = "Hi there, how are you doing today?";
 print(x[7:]);
+
+# To convert the text to uppercase 
+x = "What is your ambition in life?";
+print(x.upper());
+
+# To convert the text to lowercase
+x="WHO IS YOUR INSPIRATION IN LIFE?";
+print(x.lower());
+
+# To remove whitespace from the beginning and the end of a string
+x = "           Hi friends, what's up?            ";
+print(x.strip());
+
+# To replace a string with another string
+x = "Hi this is Drinu Kakumani from Kandukur";
+print(x.replace("Drinu", "Srinu"));
+
+# To concat two strings
+x = "Srinu";
+y = " Kakumani";
+z = x + y;
+print(z);
+
+# To add space betwwen two strings while concatenating
+x = "Srinu";
+y = "Kakumani";
+z = x + "    " + y;
+print(z);
+
+# To insert an illegal character in a string
+x = "This is \"SrinuKakumani\" from Kandukur";
+print(x);
+
+
